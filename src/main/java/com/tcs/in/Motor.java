53edb77a93e6cf43bb2f13be1723b5cbb1e37794
Gowtham <gowtham0987nm@gmail.com>
2024-11-26 15:31:23 +0530

    save

---
+++ src/main/java/com/tcs/in/Motor.java
@@ -0,0 +1,20 @@
+package com.tcs.in;
+
+public class Motor {
+	
+	public void start()
+	{
+		System.out.println("Motor Started");
+	}
+	
+	public void run()
+	{
+		System.out.println("Moto running");
+	}
+	
+	public void stop()
+	{
+		System.out.println("Moto stopped");
+	}
+
+}
